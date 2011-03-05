c67d762f12de92c7ac8bc2a496d64b6b9cf9f3b6
jeffreyantony <jeffreyantony@gmail.com>
2011-03-05 15:45:23 +0530

    Added avrdude command

--- load.py
+++ load.py
@@ -1,7 +1,9 @@
 import serial, time
+import os, sys
 fd = serial.Serial(port='/dev/ttyUSB0', baudrate=19200)
 print fd
 fd.setDTR(1)
-time.sleep(0.1)
+time.sleep(.1)
 fd.setDTR(0)
-
+cmd = 'avrdude -b 19200 -P /dev/ttyUSB0 -pm32 -c stk500v1 -U ' + sys.argv[1]
+os.system(cmd)
